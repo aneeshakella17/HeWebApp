bb18fc53ed6188310723b391cbf3e1b191436c35
Aneesh <aneakell@cisco.com>
2019-07-09 13:23:51 -0700

    finished my first draft

---
+++ static/js/index.js
@@ -0,0 +1,38 @@
+
+
+function readURL(input){
+  if (input.files && input.files[0]) {
+     var reader = new FileReader();
+     formdata = new FormData();
+     reader.onload = function(e) {
+
+        $('#blah').attr('src', e.target.result).width(150)
+                    .height(200);
+         formdata.append('image',  e.target.result);
+         $.ajax({
+            type: "POST",
+            url: "/send",
+		    data: formdata,
+		    cache: false,
+		    processData: false,
+		    contentType: false
+         }).done(function(response){
+            console.log("HI");
+            console.log(response);
+            $('#output').attr('src', response).width(150)
+                    .height(200);
+            document.getElementById("download").disabled=false;
+            document.getElementById("download").style.display = "inline-block";
+          });
+
+     }
+     reader.readAsDataURL(input.files[0]);
+     document.getElementById("arrow").style.display = "inline-block";
+  }
+
+
+}
+
+
+
+
