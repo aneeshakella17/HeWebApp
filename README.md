4c289c8c94389ae10c406cd4b067cb8e2940f603
Aneesh <aneakell@cisco.com>
2019-07-09 22:03:32 -0700

    adfa

--- README.md
+++ README.md
@@ -10,4 +10,12 @@ Output Image:
 <img src = "/static/images/forest_output.jpg" width ="400" height = "300"/>
 
 Here is my [web app](http://aneeshakella17.pythonanywhere.com/) to run Kaiming He's algorithm for dehazing hazy images.
+The app was built using Flask, Javascript, CSS, and HTML. He theorized that in a hazy image, the brightest pixels in the RGB channels are
+primarily contributed by airlight. We can use the dark channel prior to estimate the atmosphere
+vector which can be used to estimate the transmission.
 
+Example of the Dark Channel Prior
+<img src = "/static/images/dark_channel_prior.jpg" width ="600" height = "300"/>
+
+Transmission
+<img src = "/static/images/transmission.jpg" width ="400" height = "300"/>
--- static/js/index.js
+++ static/js/index.js
@@ -6,10 +6,10 @@ function readURL(input){
      var reader = new FileReader();
      formdata = new FormData();
      reader.onload = function(e) {
-
         $('#blah').attr('src', e.target.result).width(150)
                     .height(200);
-         formdata.append('image',  e.target.result);
+
+        formdata.append('image',  $('#blah').attr('src'));
          $.ajax({
             type: "POST",
             url: "/send",
@@ -17,14 +17,14 @@ function readURL(input){
 		    cache: false,
 		    processData: false,
 		    contentType: false
-         }).done(function(response){
+         }).success(function(response){
             console.log("HI");
             console.log(response);
             $('#output').attr('src', response).width(150)
                     .height(200);
           });
-
      }
+
      reader.readAsDataURL(input.files[0]);
      document.getElementById("arrow").style.display = "inline-block";
   }
